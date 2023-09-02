deff810c35efca73e845574b57ab9676952f6299
Moathalkhouly <moath.f.alkhouly@gmail.com>
2023-09-03 01:30:50 +0300

    color

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-const y = document.getElementById('y');
+const y = document.getElementById("y");
 
-document.getElementById('seats').addEventListener('change', () => {
-  document.getElementById('x').classList.toggle('d-none');
+document.getElementById("seats").addEventListener("change", () => {
+  document.getElementById("x").classList.toggle("d-none");
 });
 
-document.getElementById('return-back').addEventListener('change', () => {
-  document.getElementById('pick-up').classList.toggle('d-none');
+document.getElementById("return-back").addEventListener("change", () => {
+  document.getElementById("pick-up").classList.toggle("d-none");
 });
 
 let a;
 let b;
 
 // Hidden inputs
-y.addEventListener('change', () => {
+y.addEventListener("change", () => {
   if (y.value >= 3) {
-    document.getElementById('a1').classList.add('d-none');
-    document.getElementById('a2').classList.add('d-none');
-    a = document.getElementById('a1').value;
-    b = document.getElementById('a2').value;
+    document.getElementById("a1").classList.add("d-none");
+    document.getElementById("a2").classList.add("d-none");
+    a = document.getElementById("a1").value;
+    b = document.getElementById("a2").value;
 
-    document.getElementById('a1').value = null;
-    document.getElementById('a2').value = null;
+    document.getElementById("a1").value = null;
+    document.getElementById("a2").value = null;
   } else {
-    document.getElementById('a1').classList.remove('d-none');
-    document.getElementById('a2').classList.remove('d-none');
-    document.getElementById('a1').value = a;
-    document.getElementById('a2').value = b;
+    document.getElementById("a1").classList.remove("d-none");
+    document.getElementById("a2").classList.remove("d-none");
+    document.getElementById("a1").value = a;
+    document.getElementById("a2").value = b;
   }
 });
 
@@ -46,3 +46,18 @@ y.addEventListener('change', () => {
 //     document.getElementById('a2').value = b;
 //   }
 // });
+document.addEventListener("DOMContentLoaded", function () {
+  var inputElement = document.getElementById("address1");
+  var calendarIcon = document.getElementById("calendar-icon");
+
+  calendarIcon.addEventListener("click", function () {
+    inputElement.focus();
+    inputElement.click();
+  });
+
+  $("#address1").datepicker();
+  $("#address2").datetimepicker({
+    dateFormat: "dd-mm-yy",
+    timeFormat: "HH:mm:ss",
+  });
+});
